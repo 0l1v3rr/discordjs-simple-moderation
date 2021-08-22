19a042f183afdad7514da3d17bc2babb7105d418
Olivér <oliver.mrakovics@gmail.com>
2021-08-22 11:12:12 +0200

    Updated to Discord.js v13

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client({ partials: ["MESSAGE", "USER", "REACTION"]});
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES], partials: ["MESSAGE", "USER", "REACTION"] });
 const warns = require("./warns.json");
 const fs = require('fs');
 const ms = require('ms');
 
 bot.on("ready", async () => {
     console.log('The bot is active.');
-    bot.user.setActivity('!mute', {type: 'LISTENING'});
+    bot.user.setActivity('Moderate', {type: 'LISTENING'});
 });
 
-bot.on('message', async message => {
+bot.on('messageCreate', async message => {
     if (message.author.bot || message.channel.type === 'dm') return;
     let prefix = '!';
     let messageArray = message.content.split(" ");
@@ -24,39 +24,39 @@ bot.on('message', async message => {
     //BAN COMMAND
     //!ban @Member reason
     if (cmd === `${prefix}ban`) {
-        if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("You do not have permission to do that.");
+        if(!message.member.permissions.has("BAN_MEMBERS")) return message.reply("You do not have permission to do that.");
         const user = message.mentions.users.first();
         if (!user) return message.reply("Please specify someone you want to ban. **!ban <user> [reason]**");
         if(user.id === message.author.id) return message.reply("You cannot ban yourself.");
         const reason = args.slice(1).join(" ");
         message.guild.members.cache.get(user.id).ban({reason: reason});
  
-        const banmessage = new Discord.MessageEmbed()
+        const banmessage = new MessageEmbed()
         .setColor("#00aaaa")
         .setDescription(`${user} has been banned. Reason: **${reason != "" ? reason : "-"}**`);
-        message.channel.send(banmessage);
+        message.channel.send({ embeds: [banmessage] });
     }
 
     //KICK COMMAND
     //!kick @Member reason
     if (cmd === `${prefix}kick`) {
-        if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("You do not have permission to do that.");
+        if(!message.member.permissions.has("KICK_MEMBERS")) return message.reply("You do not have permission to do that.");
         const user = message.mentions.users.first();
         if (!user) return message.reply("Please specify someone you want to kick. **!kick <user> [reason]**");
         if(user.id === message.author.id) return message.reply("You cannot kick yourself.");
         const reason = args.slice(1).join(" ");
         message.guild.members.cache.get(user.id).kick(reason);
  
-        const kickmessage = new Discord.MessageEmbed()
+        const kickmessage = new MessageEmbed()
         .setColor("#00aaaa")
         .setDescription(`${user} has been kicked. Reason: **${reason != "" ? reason : "-"}**`);
-        message.channel.send(kickmessage);
+        message.channel.send({ embeds: [kickmessage] });
     }
 
     //MUTE COMMAND
     //!mute @Member time(s, m, d, h) reason
     if (cmd === `${prefix}mute`) {
-        if(!message.member.hasPermission("MUTE_MEMBERS")) return message.reply("You do not have permission to do that.");
+        if(!message.member.permissions.has("MUTE_MEMBERS")) return message.reply("You do not have permission to do that.");
         const user = message.mentions.users.first();
         if (!user) return message.reply("Please specify someone you want to mute. **!mute <user> [time] [reason]**");
         const target = message.guild.members.cache.get(user.id);
@@ -69,24 +69,25 @@ bot.on('message', async message => {
         if (!time) time = "1h";
 
         target.roles.add(mutedrole.id);
-        const mute = new Discord.MessageEmbed()
+        const embed = new MessageEmbed()
         .setColor("#00aaaa")
-        .setDescription(`${user} has been muted by ${message.author} for ${ms(ms(time))}.\nReason: **${reason != "" ? reason : "-"}**`);
-        message.channel.send(mute);
+        .setDescription(`${user} has been muted by ${message.author} for ${ms(ms(time))}.\nReason: **${reason != "" ? reason : "-"}**`)
 
+        message.channel.send({ embeds: [embed] });
+        
         setTimeout(() => {
             target.roles.remove(mutedrole.id);
-            const unmute = new Discord.MessageEmbed()
+            const unmute = new MessageEmbed()
             .setColor("#00aaaa")
             .setDescription(`${user} has been unmuted.`);
-            message.channel.send(unmute);
+            message.channel.send({ embeds: [unmute] });
         }, ms(time));
     }
 
     //UNMUTE COMMAND
     //!unmute @Member reason
     if (cmd === `${prefix}unmute`) {
-        if(!message.member.hasPermission("MUTE_MEMBERS")) return message.reply("You do not have permission to do that.");
+        if(!message.member.permissions.has("MUTE_MEMBERS")) return message.reply("You do not have permission to do that.");
         const user = message.mentions.users.first();
         if (!user) return message.reply("Please specify someone you want to unmute. **!unmute <user> [reason]**");
         const target = message.guild.members.cache.get(user.id);
@@ -96,16 +97,16 @@ bot.on('message', async message => {
 
         const reason = args.slice(1).join(" ");
         target.roles.remove(mutedrole.id);
-        const unmute = new Discord.MessageEmbed()
+        const unmute = new MessageEmbed()
         .setColor("#00aaaa")
         .setDescription(`${user} has been unmuted by ${message.author}.\nReason: **${reason != "" ? reason : "-"}**`);
-        message.channel.send(unmute);
+        message.channel.send({ embeds: [unmute] });
     }
 
     //WARN COMMAND
     //!warn @Member reason
     if (cmd === `${prefix}warn`) {
-        if(!message.member.hasPermission("MUTE_MEMBERS")) return message.reply("You do not have permission to do that.");
+        if(!message.member.permissions.has("MUTE_MEMBERS")) return message.reply("You do not have permission to do that.");
         const user = message.mentions.users.first();
         if (!user) return message.reply("Please specify someone you want to warn. **!warn <user> [reason]**");
         const target = message.guild.members.cache.get(user.id);
@@ -123,27 +124,27 @@ bot.on('message', async message => {
         }
 
         if(warns[user.id].warnCount >= 5) {
-            const mute = new Discord.MessageEmbed()
+            const mute = new MessageEmbed()
             .setColor("#00aaaa")
             .setDescription(`${user} has been muted. (**5**/**5**)\nReason: **${reason != "" ? reason : "-"}**`);
-            message.channel.send(mute);
+            message.channel.send({ embeds: [mute] });
             
             target.roles.add(mutedrole.id);
             warns[user.id].warnCount = 0;
     
             setTimeout(() => {
                 target.roles.remove(mutedrole.id);
-                const unmute = new Discord.MessageEmbed()
+                const unmute = new MessageEmbed()
                 .setColor("#00aaaa")
                 .setDescription(`${user} has been unmuted.`);
-                message.channel.send(unmute);
+                message.channel.send({ embeds: [unmute] });
             }, 1000 * 900);
 
         } else {
-            const warn = new Discord.MessageEmbed()
+            const warn = new MessageEmbed()
             .setColor("#00aaaa")
             .setDescription(`${user} has been warned by ${message.author}. (**${warns[user.id].warnCount}**/**5**) \nReason: **${reason != "" ? reason : "-"}**`);
-            message.channel.send(warn);
+            message.channel.send({ embeds: [warn] });
         }
 
         fs.writeFile("./warns.json", JSON.stringify(warns), err => {
